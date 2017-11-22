7b872ef56946ff37c00dbb616af744346c2d19c5
Jose Macchi <jmacchi@sparkdigital.com>
2017-11-22 16:04:24 -0300

    Credits for a range of cards

--- getcredit.py
+++ getcredit.py
@@ -1,12 +1,19 @@
 import urllib2
 import datetime
 
-card = 10026324
 now = datetime.datetime.now()
-response = urllib2.urlopen('http://www.gpssumo.com/movimientos/get_movimientos/'+str(card)+'/3')
-rslt = response.read()
-
-line = str(card)+','+rslt[rslt.find('saldo : ')+11:rslt.find(', saldo_viajes :')-1]
 file = open('results/credit-'+str(now.date())+'.txt','w')
-file.write(line)
+
+fromcard = 10000000
+#tocard = 10250000
+tocard = 10000010
+
+for x in range(fromcard, tocard):
+    card = x
+    response = urllib2.urlopen('http://www.gpssumo.com/movimientos/get_movimientos/'+str(card)+'/3')
+    rslt = response.read()
+    line = str(card)+','+rslt[rslt.find('saldo : ')+11:rslt.find(', saldo_viajes :')-1]+'\n'
+    file.write(line)
+    print(x)
+
 file.close()
