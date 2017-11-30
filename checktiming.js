2012eb7f8ac14922bdee3f627305636c44af043a
Jose Macchi <jose.macchi@gmail.com>
2017-11-30 17:34:37 -0300

    LineCheck function ok!

--- checktiming.js
+++ checktiming.js
@@ -100,6 +100,7 @@ var _busLines = {
     }
 };
 
+var _intervalBetweenBuses = 10*60*1000;  // 10 min
 var _intervalCheck = 5000;
 var _maxLogsByProx = 5;
 
@@ -114,7 +115,7 @@ function lineCheck(line) {
         if (!error && response.statusCode == 200) {
             arrBuses = eval(body);
             var distances = [];
-            var proximityLog = systemState.proximityLog[line];
+            var proximityLog = (systemState.proximityLog[line])?systemState.proximityLog[line]:[];
             var when = Date.now();
             var counter = 0;
             while (counter < arrBuses[1].length) {
@@ -134,24 +135,27 @@ function lineCheck(line) {
                     );
                     var beaconId = _busLines[line].beaconList[bcounter].id;
                     if (isInsideBeacon) {
-                        console.log('Bus ' + aBusId +' ('+line+') in proximity of beacon ' + beaconId);
-                        console.log(proximityLog);
-                        console.log('Size: '+proximityLog[beaconId].length);
+                        //console.log('Bus ' + aBusId +' ('+line+') in proximity of beacon ' + beaconId);
                         // Save  { time, busLine, busId, beaconId, distanceToBeacon } to proximityLog
                         if (proximityLog[beaconId] == null) {
                             proximityLog[beaconId] = [];
                         }
                         if (proximityLog[beaconId].length > 0) {
                             // Check if it's same Bus as last recorded Bus
-                            if (proximityLog[beaconId][0].line != line &&
-                                proximityLog[beaconId][0].bus != aBusId && 
-                                proximityLog[beaconId][0].beacon != beaconId) {
+                            if (proximityLog[beaconId][0].bus != aBusId.toString()) {
+                                    //console.log('Inserting new proximities (it is a different bus)');
                                     if (proximityLog[beaconId].length == _maxLogsByProx) {
                                         proximityLog[beaconId].pop();
                                     }
                                     proximityLog[beaconId].unshift({ time: when, line: line, bus: aBusId, beacon: beaconId, dist: d });   
                             }
+                        } else {
+                            //console.log('Inserting the first proximity');
+                            // It's the first one
+                            proximityLog[beaconId].push({ time: when, line: line, bus: aBusId, beacon: beaconId, dist: d });
                         }
+                        //console.log(proximityLog);
+                        //console.log('------------------------------------');
                     }
                     // Distances to beacons at last check
                     distances.push({ time: when, line: line, bus: aBusId, beacon: beaconId, dist: d });
@@ -159,17 +163,46 @@ function lineCheck(line) {
                 }
                 ++counter;
             }
-            //console.log(distances);
             systemState.distances[line] = distances;
             systemState.proximityLog[line] = proximityLog;
-            //console.log(systemState.proximityLog[line]);            
         }
     }
     request.post(_busLines[line].req, callback);
 }
 
+var tprox= [];
+tprox['test-1'] = [ { time: 1512169401035, line: '500', bus: '13', beacon: 'test-1', dist: 14 },
+  { time: 1512069250837, line: '500', bus: '9', beacon: 'test-1',  dist: 99 },
+  { time: 1512068840522, line: '500', bus: '12',beacon: 'test-1',  dist: 15 },
+  { time: 1512068690417, line: '500', bus: '8', beacon: 'test-1',  dist: 21 },
+  { time: 1512068315108, line: '500', bus: '11',beacon: 'test-1',  dist: 89 } ];
+tprox['test-3'] = [ { time: 1512069405946, line: '500', bus: '8', beacon: 'test-3', dist: 9 },
+  { time: 1512068680420, line: '500', bus: '6', beacon: 'test-3', dist: 35 },
+  { time: 1512068220032, line: '500', bus: '4', beacon: 'test-3', dist: 92 } ];
+tprox['test-2'] = [ { time: 1512069516049, line: '500', bus: '2', beacon: 'test-2', dist: 67 },
+  { time: 1512069075708, line: '500', bus: '8', beacon: 'test-2', dist: 70 },
+  { time: 1512068875545, line: '500', bus: '13',beacon: 'test-2', dist: 67 },
+  { time: 1512068340121, line: '500', bus: '12',beacon: 'test-2', dist: 56 },
+  { time: 1512068335140, line: '500', bus: '6', beacon: 'test-2', dist: 48 } ] ;
+
+systemState.proximityLog['500'] = tprox ;
+
 function checkTiming() {
-    systemState.proximityLog;
+    var pLog = systemState.proximityLog ;
+    var cline = 0;
+    while (cline < pLog.length) {
+        byBeaconLog = pLog[cline];
+        var cbeacon = 0;
+        while (cbeacon < byBeaconLog.length) {
+            if (cbeacon+1 < 5) {
+                if ( ((byBeaconLog[cbeacon].time - byBeaconLog[cbeacon+1].time)/1000) > _intervalBetweenBuses ){
+                    console.log('Timing Alert !!! - Delayed: ' + byBeaconLog[0]);
+                }
+            }
+            ++cbeacon;
+        } 
+        ++cline;
+    };
 }
 
 function saveStatus() {
@@ -182,9 +215,9 @@ function saveStatus() {
 }
 
 setInterval(function () { lineCheck('500') }, _intervalCheck);
-/*setInterval(function() { lineCheck('501') },_intervalCheck);
+setInterval(function() { lineCheck('501') },_intervalCheck);
 setInterval(function() { lineCheck('502') },_intervalCheck);
 setInterval(function() { lineCheck('503') },_intervalCheck);
 setInterval(function() { lineCheck('504') },_intervalCheck);
-setInterval(function() { lineCheck('505') },_intervalCheck);*/
-//setInterval(saveStatus,_intervalCheck+100);
+setInterval(function() { lineCheck('505') },_intervalCheck);
+setInterval(checkTiming,_intervalCheck*3);
